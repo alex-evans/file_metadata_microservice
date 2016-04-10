7dbc9c0e3b5ce9ae9cade49dd2eefc2be798e12a
Alex Evans <alex.evans.d@gmail.com>
2016-04-10 02:26:22 +0000

    HTML up and running, needs to be cleaned up to respect buttons

---
+++ app/routes/index.js
@@ -0,0 +1,22 @@
+'use strict';
+
+var path = process.cwd();
+// var ImgHandler = require(path + '/app/controllers/imgHandler.js');
+
+module.exports = function (app, db) {
+	
+ 	// var imgHandler = new ImgHandler(db);
+	
+	app.route('/')
+		.get(function(req,res) {
+			res.sendFile(path + '/public/index.html');
+		});
+/*		
+	app.route('/api/imagesearch/:search')
+		.get(imgHandler.getImages);
+		
+	app.route('/api/latest/imagesearch')
+		.get(imgHandler.getSearches);
+*/
+};
+
---
+++ server.js
@@ -0,0 +1,27 @@
+'use strict';
+
+var express = require('express');
+var routes = require('./app/routes/index.js');
+var mongo = require('mongodb').MongoClient;
+var app = express();
+var url = 'mongodb://localhost:27017/clementinejs';
+require('dotenv').config();
+
+mongo.connect(url, function(err,db) {
+    if(err) {
+        throw new Error('Database failed to connect!');
+    } else {
+        console.log('MongoDB successfully connected.');
+    }
+    
+    app.use('/controllers', express.static(process.cwd() + '/app/controllers'));
+    app.use('/public', express.static(process.cwd() + '/public'));
+
+    routes(app, db);
+
+    var port = process.env.PORT || 8080;
+    app.listen(port,  function () {
+	    console.log('Node.js listening on port ' + port + '...');
+    });
+
+});
